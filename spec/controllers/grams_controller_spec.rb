39ec338e9bfc5b5c3dc4f09dca8457d62c77e88a
kat-gie <gielenkatie@gmail.com>
2017-01-29 19:26:05 -0600

    added edit and update functionality

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,13 +2,39 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#update action" do
+    it "should allow users to successfully update grams" do 
+      gram = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: gram.id, gram: { message: 'Changed' }
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq "Changed"
+    end 
+
+    it "should return a 404 if the gram can't be found" do
+      patch :update, id: 'YOLOSWAG', gram: { message: 'Changed' }
+      expect(response).to have_http_status(:not_found)
+    end 
+
+    it "should render the edit form with an http status of unprocessable_entity" do 
+      gram = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: gram.id, gram: { message: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "Initial Value"
+    end 
+  end 
+
   describe "grams#edit action" do
     it "should successfully show the edit form if the gram is found" do
-
+      gram = FactoryGirl.create(:gram)
+      get :edit, id: gram.id 
+      expect(response).to have_http_status(:success)
     end 
 
     it "should return a 404 if the gram is not found" do 
-
+      get :edit, id: 'LOSERLY'
+      expect(response).to have_http_status(:not_found)
     end 
   end 
 
@@ -19,7 +45,7 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:success)
     end 
 
-    it "should return a 404 error if the gram is not found" do
+    it "should return a 404 if the gram is not found" do
       get :show, id: 'TACOCAT'
       expect(response).to have_http_status(:not_found)
     end 
